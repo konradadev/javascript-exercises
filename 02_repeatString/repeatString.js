d3370b58f422158e44fb5ae518aac443eb948a25
konradadev <konradadev@gmail.com>
2022-02-11 15:11:53 +0100

    Finish exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text,repeats) {
+    if (repeats<0){
+        return 'ERROR';
+    }
+    let textAppended='';
+    for(let i=0;i<repeats;i++){
+        textAppended+=text;
+    }
+    return textAppended;
 };
 
 // Do not edit below this line
