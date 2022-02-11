d22e05637903d1ec77dc419e3a43773614c88ffa
konradadev <konradadev@gmail.com>
2022-02-11 15:29:45 +0100

    Finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const splittedText = text.split('');
+    const reversedSplittedText = splittedText.reverse();
+    const reversedText = reversedSplittedText.join('');
+    return reversedText;
 };
 
 // Do not edit below this line
