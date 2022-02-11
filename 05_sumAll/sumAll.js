fc4cd9675aedd5fe5764b95aee27173a55adb9f0
konradadev <konradadev@gmail.com>
2022-02-11 16:52:46 +0100

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startAt, endAt) {
+
+    if(typeof(startAt) + typeof(endAt) != "numbernumber"){
+        return "ERROR";
+    }
+
+    if(startAt < 0 || endAt < 0){
+        return "ERROR";
+    }
+
+    //make startAt always smaller
+    if(startAt > endAt){
+        let helper = startAt;
+        startAt = endAt;
+        endAt = helper;
+    }
+
+    const repeats = endAt - startAt;
+    let result = 0;
+
+    for(let i=0; i <= repeats; i++){
+        result = result + (startAt + i);
+    }
+
+    return result;
 
 };
 
