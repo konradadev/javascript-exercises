1ee86135ffd73095fc2284d5c553b29ac932e63d
konradadev <konradadev@gmail.com>
2022-02-11 16:34:44 +0100

    Finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(fullArray, ...valuesToRemove) {
+    for(let i=0; i < valuesToRemove.length; i++){
+        for(let j=0; j < fullArray.length; j++){
+            if (valuesToRemove[i]===fullArray[j]){
+                fullArray.splice(j,1);
+            }
+        }
+    }
+    return fullArray;
 };
 
 // Do not edit below this line
