a6fa9f88b98b55415d1209c640b4b6f0fd0865f8
konradadev <konradadev@gmail.com>
2022-02-11 17:20:18 +0100

    Finish exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  let celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const fahrenheit = celsius * 9/5 + 32;
+  const fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
