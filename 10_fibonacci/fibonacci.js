2a30f4a7c990468ff5e6eaae64aafd5ea2cc8830
konradadev <konradadev@gmail.com>
2022-03-02 19:21:37 +0100

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n<1){
+        return "OOPS";
+    }
+    let current=1;
+    let previous=1;
+    for(let i=2; i<n; i++){
+        current=current+previous;
+        previous=current-previous;
+    }
+    return current;
 };
 
 // Do not edit below this line
