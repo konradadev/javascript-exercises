9884d9756da0f4f6d64aa73eab5633db0186cca5
konradadev <konradadev@gmail.com>
2022-03-02 19:02:46 +0100

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const trimmed = phrase.replace(/ /g,"");
+    const specialsRemoved = trimmed.replace(/[^a-zA-Z0-9 ]/g, '');
+    const finalPhrase = specialsRemoved.toLowerCase();
+    const phraseArray = finalPhrase.split("");
+    const phraseReversedArray = finalPhrase.split("").reverse();
+    let isTruthful = 0;
+    for(let i=0; i<finalPhrase.length;i++){
+        if(phraseArray[i] === phraseReversedArray[i]){
+            isTruthful++;
+        }else{
+            isTruthful--;
+        }
+    }
+    if(isTruthful===finalPhrase.length){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
