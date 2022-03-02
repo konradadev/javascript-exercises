867199726bb53b77937b4002096dd11542b56d13
konradadev <konradadev@gmail.com>
2022-03-02 22:57:33 +0100

    Finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    const peopleOldest = people.sort((a, b) => {
+        let previous=0;
+        let next=0;
+        if(!a.yearOfDeath){
+            previous = currentYear-a.yearOfBirth;
+        }else{
+            previous = a.yearOfDeath-a.yearOfBirth;
+        };
 
+        if(!b.yearOfDeath){
+            next = currentYear-b.yearOfBirth;
+        }else{
+            next = b.yearOfDeath-b.yearOfBirth;
+        };
+
+        return previous > next ? -1 : 1;
+    })
+
+    return peopleOldest[0];
 };
 
 // Do not edit below this line
