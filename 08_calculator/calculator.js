461d565d4d39318334c51a29e20e79c2db7f3fc5
konradadev <konradadev@gmail.com>
2022-03-02 16:59:11 +0100

    Add addition and subtraction

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,9 +1,9 @@
-const add = function() {
-	
+const add = function(addendA, addendB) {
+  return addendA+addendB;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend-subtrahend;
 };
 
 const sum = function() {
