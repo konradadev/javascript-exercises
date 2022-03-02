aee290a95528ab53bd195a9434fc6d57f7bbc5e8
konradadev <konradadev@gmail.com>
2022-03-02 17:26:43 +0100

    Finish exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,12 +14,22 @@ const multiply = function(factors) {
   return factors.reduce((total, factor) => total*factor, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) { 
+  let result = base;
+	for(let i=1; i<exponent;i++){
+    result*=base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result=1;
+  if(number!==0 && number!==1){
+	  for(let i=1;i<=number;i++){
+      result*=i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
